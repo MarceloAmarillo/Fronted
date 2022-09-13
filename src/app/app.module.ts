015b09afe672edaafec67cd14a69128bced96b52
MarceloAmarillo <marceloamarillo1@gmail.com>
2022-09-12 21:33:04 -0300

    integrar el back al front login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoapComponent } from './components/logoap/logoap.component';
 import { RedesComponent } from './components/redes/redes.component';
@@ -36,6 +37,8 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
